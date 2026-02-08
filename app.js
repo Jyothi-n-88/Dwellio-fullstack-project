7fdcbb1f8e2080f85a20df3c52dfc8460b172a0f
Jyothi.N <jyothin882006@gmail.com>
2026-02-09 00:12:40 +0530

    Implement routing structure for listings and reviews

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
-const Listing=require("./models/listing.js");
 const methodOverride = require("method-override");
 const app = express();
 const ejsMate=require("ejs-mate");
@@ -15,10 +14,10 @@ app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "public")));
 const MONGO_URL = process.env.MONGO_URL;
 const PORT = process.env.PORT || 8080;
-const wrapAsync=require("./utils/wrapAsync.js");
 const ExpressError=require("./utils/expressError.js");
-const { listingSchema,reviewSchema  } = require("./schema.js");
-const Review=require("./models/reviews.js");
+const listingrouter=require("./routes/listings.js")
+const reviewrouter=require("./routes/reviews.js")
+
 async function main() {
   await mongoose.connect(MONGO_URL);
 }
@@ -34,125 +33,9 @@ main()
 app.get("/", (req, res) => {
   res.send("Server is running 🚀");
 });
-// app.get("/testingModel",async(req,res)=>
-// {
-//   let sampleListing=new Listing({
-//     title:"Villa in Mysore",
-//     description:"Casa Luxora 2BHK|Premium|Ideal",
-//     price:1800,
-//     location:"Mysore Circle",
-//     country:"India",
-//     image:""
-//   });
-//   await sampleListing.save();
-//   console.log(sampleListing);
-//   res.send("Successful Test");
-// });
-const validateListing = (req, res, next) => {
-  const { error } = listingSchema.validate(req.body);
-  if (error) {
-    const msg = error.details.map(el => el.message).join(", ");
-    throw new ExpressError(400, msg);
-  }
-  next();
-};
 
-const validateReview = (req, res, next) => {
-  const { error } = reviewSchema.validate(req.body);
-  if (error) {
-    const msg = error.details.map(el => el.message).join(",");
-    throw new ExpressError(400, msg);
-  }
-  next();
-};
-
-app.get("/listings",wrapAsync( async (req, res) => {
-  const allListings = await Listing.find({});
-  res.render("listings/index.ejs", { allListings });
-}));
-
-//New Route
-app.get("/listings/new", wrapAsync(async(req, res) => {
-  res.render("listings/new.ejs");
-}));
-
-//Show Route
-app.get("/listings/:id", wrapAsync(async (req, res) => {
-  const { id } = req.params;
-  const listing = await Listing.findById(id).populate("reviews");
-
-  if (!listing) {
-    throw new ExpressError(404, "Listing not found");
-  }
-
-  res.render("listings/show.ejs", { listing });
-}));
-
-//Create Route
-app.post(
-  "/listings",validateListing,
-  wrapAsync(async (req, res) => {
-    const newListing = new Listing(req.body.listing);
-    await newListing.save();
-    res.redirect("/listings");
-  })
-);
-
-
-//Edit Route
-app.get("/listings/:id/edit", wrapAsync(async (req, res) => {
-  let { id } = req.params;
-  const listing = await Listing.findById(id);
-  res.render("listings/edit.ejs", { listing });
-}));
-
-//Update Route
-app.put("/listings/:id", validateListing,wrapAsync(async (req, res) => {
-  let { id } = req.params;
-  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
-  res.redirect(`/listings/${id}`);
-}));
-
-//Delete Route
-app.delete("/listings/:id",wrapAsync( async (req, res) => {
-  let { id } = req.params;
-  let deletedListing = await Listing.findByIdAndDelete(id);
-  console.log(deletedListing);
-  res.redirect("/listings");
-}));
-
-app.post(
-  "/listings/:id/reviews",
-  validateReview,
-  wrapAsync(async (req, res) => {
-    const listing = await Listing.findById(req.params.id);
-    const review = new Review(req.body.review);
-
-    listing.reviews.push(review);
-
-    await review.save();
-    await listing.save();
-
-    res.redirect(`/listings/${listing._id}`);
-  })
-);
-
-app.delete(
-  "/listings/:id/reviews/:reviewId",
-  wrapAsync(async (req, res) => {
-    const { id, reviewId } = req.params;
-
-    // 1️⃣ Remove review reference from listing
-    await Listing.findByIdAndUpdate(id, {
-      $pull: { reviews: reviewId }
-    });
-
-    // 2️⃣ Delete review document itself
-    await Review.findByIdAndDelete(reviewId);
-
-    res.redirect(`/listings/${id}`);
-  })
-);
+app.use("/listings",listingrouter);
+app.use("/listings/:id/reviews",reviewrouter);
 
 app.use( (req, res, next) => {
   next(new ExpressError(404, "Page Not Found!"));
@@ -162,7 +45,6 @@ app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Something went wrong!";
   res.status(statusCode).render("error.ejs", { message });
-
 });
 
 app.listen(PORT, () => {
---
+++ routes/reviews.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const router=express.Router({ mergeParams: true });
+const {reviewSchema  } = require("../schema.js");
+const wrapAsync=require("../utils/wrapAsync.js");
+const ExpressError=require("../utils/expressError.js");
+const Review=require("../models/reviews.js");
+const Listing=require("../models/listing.js");
+
+const validateReview = (req, res, next) => {
+  const { error } = reviewSchema.validate(req.body);
+  if (error) {
+    const msg = error.details.map(el => el.message).join(", ");
+    throw new ExpressError(400, msg);
+  }
+  next();
+};
+
+router.post(
+  "/",
+  validateReview,
+  wrapAsync(async (req, res) => {
+    const listing = await Listing.findById(req.params.id);
+    if (!listing) {
+        throw new ExpressError(404, "Listing not found");
+    }
+    const review = new Review(req.body.review);
+
+    listing.reviews.push(review);
+
+    await review.save();
+    await listing.save();
+
+    res.redirect(`/listings/${listing._id}`);
+  })
+);
+
+router.delete(
+  "/:reviewId",
+  wrapAsync(async (req, res) => {
+    const { id, reviewId } = req.params;
+
+    // 1️⃣ Remove review reference from listing
+    await Listing.findByIdAndUpdate(id, {
+      $pull: { reviews: reviewId }
+    });
+
+    // 2️⃣ Delete review document itself
+    await Review.findByIdAndDelete(reviewId);
+
+    res.redirect(`/listings/${id}`);
+  })
+);
+module.exports = router;
